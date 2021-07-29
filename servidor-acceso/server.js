0d2a27417512dee7f7f16f5468b51785f7c46dd2
Armando Ibarrarán <armando@kali.kali>
2021-07-29 14:21:39 -0500

    Connectory 1.1

--- servidor-acceso/server.js
+++ servidor-acceso/server.js
@@ -11,7 +11,9 @@ const HTTP_PORT = 8000;
 
 //Retrieves html files to the designated path
 app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname, './index.html')));
-app.get('/read/',(req,res)=>res.sendFile(path.resolve(__dirname, './read/index.html')));
+app.get('/escanear/',(req,res)=>res.sendFile(path.resolve(__dirname, './escanear/index.html')));
+app.get('/registro/',(req,res)=>res.sendFile(path.resolve(__dirname, './registro/index.html')));
+app.get('/eventos/',(req,res)=>res.sendFile(path.resolve(__dirname, './eventos/index.html')));
 app.use('/assets', express.static('assets'))
 
 //HTTPS is required by chrome to grant camera permission
